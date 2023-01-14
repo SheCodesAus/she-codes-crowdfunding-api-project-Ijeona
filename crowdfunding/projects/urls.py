29772971c8851369a1edb9e2458989bab29447b1
Ijeona <Ijeona@users.noreply.github.com>
2023-01-14 12:56:29 +0800

    Set up serialiser form for pledges

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -5,7 +5,8 @@ from. import views
 
 urlpatterns = [
     path('projects/',views.ProjectList.as_view(),name="project-list"),
-    path('projects/<int:pk>/',views.ProjectDetail.as_view(), name="projects-detail")
+    path('projects/<int:pk>/',views.ProjectDetail.as_view(), name="projects-detail"),
+    path('pledges/', views.PledgeList.as_view(), name='pledge-list')
 ]
 
 urlpatterns = format_suffix_patterns (urlpatterns)
