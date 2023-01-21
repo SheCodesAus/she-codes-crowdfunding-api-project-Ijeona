b76184a1c9661c776c65cf4ed285e5dffb52ba85
Ijeona <Ijeona@users.noreply.github.com>
2023-01-21 10:35:16 +0800

    merge and paths

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -18,6 +18,8 @@ from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",include('projects.urls'))
+    path('', include('projects.urls')),
+	path('users/', include('users.urls')),
+	path('api-auth/', include('rest_framework.urls')),
+	path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]
